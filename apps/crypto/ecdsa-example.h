ee7549a1a7b311fbd6ad4e86b5312482ca9195f2
seftiz <zohar_sefti@yahoo.com>
2016-10-10 16:02:22 +0300

    remove files

--- apps/crypto/ecdsa-example.h
+++
@@ -1,8 +0,0 @@
-#ifndef __APPS_ECDSA_EXAMPLE__
-#define __APPS_ECDSA_EXAMPLE__
-
-#include <libcli.h>
-
-int cli_ecdsa_example( struct cli_def *cli, const char *command, char *argv[], int argc );
-
-#endif
--- tg/asn1c.sed
+++
@@ -1,2 +0,0 @@
-s@^\([A-Z][a-zA-Z]*\)\( .* SEQUENCE .* \)OF SEQUENCE {@\1\2OF \1Seq\r\n\1Seq ::= SEQUENCE {@g
-s@netinet/in.h@nxd_bsd.h@g
